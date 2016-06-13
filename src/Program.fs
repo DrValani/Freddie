95fa4a0d8fdccbc043f81b226b283f4e0e487db7
John Donnellan <john@blueTribu.com>
2016-06-13 10:09:03 +0100

    start creating terrain verticies map

---
+++ src/Program.fs
@@ -0,0 +1,7 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv = 
+    Terrain.go ()
+    Console.ReadKey() |> ignore
+    0
---
+++ src/Terrain.fs
@@ -0,0 +1,45 @@
+﻿module Terrain
+
+open System
+
+let randMinusToPlusOne (random : Random) =
+    random.NextDouble() * 2.0 - 1.0
+    
+       
+let nudge random range initial =
+    initial + (range * randMinusToPlusOne random)
+
+
+let printMap (heights : float[,]) =
+    let upper = heights.GetLength(0) - 1
+    for y in [0..upper] do
+        for x in [0..upper] do
+            printf "%f " heights.[x, y]
+        printfn ""
+    
+let createVerticiesMap random exp =
+    let tileCount = pown 2 exp
+    let vericiesCount = tileCount + 1
+    let heights = Array2D.zeroCreate vericiesCount vericiesCount
+
+    let initialCornerHeight = 0.5
+    let nudgeRange = 0.25
+    let newCornerHeight () = 
+        nudge random nudgeRange initialCornerHeight
+
+    for x in [0; tileCount] do
+        for y in [0; tileCount] do
+            heights.[x, y] <- newCornerHeight ()
+
+    heights
+
+
+let random =
+    let seed = Random().Next()
+    printfn "Seed: %d" seed
+    Random(seed)
+
+let go () =
+    createVerticiesMap random 3
+    |> printMap
+
