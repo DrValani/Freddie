8ad5030f670b86c88e06575a9a988492829d541d
John Donnellan <john@blueTribu.com>
2016-06-22 12:35:46 +0100

    add alternate 'unfold' implemenation of improveIncrementally

--- src/Common.fs
+++ src/Common.fs
@@ -7,5 +7,11 @@ let improveIncrementally guess improve isGoodEnough =
             let improved = improve state
             yield! improveUntilDone improved }
     guess |> improveUntilDone  
-    
-   
+
+
+// Alternate implementation using unfold.
+let improveIncrementally2 guess improve isGoodEnough =
+    guess
+    |> Seq.unfold (fun state ->
+        if isGoodEnough state then None
+        else Some (state, improve state))
